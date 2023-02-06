11f9a09290fb558b47c24082bb9b7c7413787aff
Joel <ndohjoelmbj16@gmail.com>
2023-02-06 21:40:19 +0100

    I have just done the neccessary flow from create quiz for a single user quiz type

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,30 @@
 let h3Tag = document.querySelector('div.user-name-section span h3');
 let h5Tag = document.querySelector('div.user-name-section h5');
+let createQuizBtn = document.querySelector("button.create-quiz-btn")
+let joinQuiz = document.querySelector("buttoon.join-quiz-btn")
+let profileDiv = document.querySelector("div.profile-div")
+let sendCreateQuizRequestBtn = document.getElementById("create-quiz-send-request")
+
+const quizTypeRadioButton = document.querySelectorAll('input[name="quiz-type"]');
+const nextAfterQuizTypeBtn = document.getElementById('next-after-quiz-type');
+
+const quizDifficultyRadioButton = document.querySelectorAll('input[name="quiz-difficulty"]');
+const nextAfterQuizDifficultyBtn = document.getElementById('next-after-quiz-difficulty');
+
+let isQuizTypeRadioSelected = false;
+let isQuizDifficultyRadioSelected = false;
+
+let quizTypeModal = document.querySelector("#quiz-type")
 
 let tbody = document.querySelector('.quiz-history-section tbody');
+
+const quizCategoryDropDown = document.getElementById("quiz-category-dropdown");
+
+
+let category
+let difficulty
+let expectedParticipants = 1
+let quizType = "single"
 
 
 window.addEventListener('load', () => {
@@ -122,6 +145,13 @@ function getAllCategories(){
     })
     .then(result =>{        
         localStorage.setItem("categories", JSON.stringify(result.data))
+        categories = result.data
+        categories.forEach(category => {
+            const option = document.createElement("option");
+            option.textContent = category.name;
+            option.value = category.id;
+            quizCategoryDropDown.appendChild(option);
+        });
     })
     .catch(error => {
         alert(error)
@@ -157,5 +187,86 @@ function getAllDifficulties(){
     .catch(error => {
         alert(error)
     });
+
+}
+
+function closeModal(element){
+    element.parentElement.parentElement.classList.toggle("activeInfo")
+}
+
+function previousModal(element){
+    document.getElementById(element.getAttribute('data-previous')).classList.toggle('activeInfo')
+}
+
+function toggleProfileDiv(){
+    profileDiv.classList.toggle("invisible")
+}
+
+createQuizBtn.addEventListener("click", () => {
+    quizTypeModal.classList.toggle("activeInfo")
+})
+
+quizTypeRadioButton.forEach(radioButton => {
+    radioButton.addEventListener('click', function() {
+      isQuizTypeRadioSelected = true;
+      nextAfterQuizTypeBtn.removeAttribute('disabled');
+      nextAfterQuizTypeBtn.setAttribute('data-next', radioButton.getAttribute('data-next'));
+    });
+});
+  
+nextAfterQuizTypeBtn.addEventListener('click', function() {
+    if (!isQuizTypeRadioSelected) {
+        nextAfterQuizTypeBtn.setAttribute('disabled', '');
+    }
+});
+
+function nextModal(element){
+    document.getElementById(element.getAttribute('data-next')).classList.toggle('activeInfo')
+}
+
+ 
+quizDifficultyRadioButton.forEach(radioButton => {
+    radioButton.addEventListener('click', function() {
+        isQuizDifficultyRadioSelected = true;
+      nextAfterQuizDifficultyBtn.removeAttribute('disabled');
+    });
+});
+  
+nextAfterQuizDifficultyBtn.addEventListener('click', function() {
+    if (!isQuizTypeRadioSelected) {
+        nextAfterQuizDifficultyBtn.setAttribute('disabled', '');
+    }
+});
+
+
+function setQuizType(){
+    for (let i = 0; i < quizTypeRadioButton.length; i++) {
+        if (quizTypeRadioButton[i].checked) {
+            quizType = quizTypeRadioButton[i].value;
+            break;
+        }
+    }
+    localStorage.setItem("quizType", quizType)
+}
+
+function setQuizDifficulty(){
+    for (let i = 0; i < quizDifficultyRadioButton.length; i++) {
+        if (quizDifficultyRadioButton[i].checked) {
+            difficulty = quizDifficultyRadioButton[i].value;
+            break;
+        }
+    }
+
+    localStorage.setItem("difficulty", difficulty)
+}
+
+function setExpectedParticipants(){
+
+}
+
+function setCategories(){
+    category = quizCategoryDropDown.options[quizCategoryDropDown.selectedIndex].value;
+
+    localStorage.setItem("category", category)
 
 }
